fc78a5f50db1ee4f259ae99d5a4edf768c8322bf
victormelladobuvinic-cpu <victor.melladobuvinic@gmail.com>
2025-12-30 22:32:40 -0300

    rendirazo de datos al apretar elboton

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 import { menuArray} from './data.js'
-
+let orderArray = []
 function getMenuHtml(menu) {
 
 return menu.map(({name, ingredients, price, emoji, id}) =>{
@@ -31,16 +31,32 @@ function handleAddClick(menu) {
 
         if(e.target.id) {
 
-
-          document.getElementById('checkout-container').style.display = 'block'; 
-         
-             const addItem = menu.find(function(item) { 
+            document.getElementById('checkout-container').classList.add('visible')
+                    
+              const targetItem = menu.find(function(item) { 
                 return item.id === Number(e.target.id)
             })
-            console.log(addItem.name)
-        }
+            
+            orderArray.push(targetItem)
+            renderOrder()
+        } 
+
+
 
     })
     
 }
-handleAddClick(menuArray)
+ function renderOrder() {
+
+    let orderHtml = orderArray.map(({name, price}) => {
+        return `
+            
+            <div class="checkout-item">
+                <div class="checkout-item-name black">${name}</div>
+                <div class="checkout-item-price black">$${price}</div>
+            </div>`
+    }).join('')
+
+    document.getElementById('checkout-section').innerHTML = orderHtml
+ }
+ handleAddClick(menuArray)
